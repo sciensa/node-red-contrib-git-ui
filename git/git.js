7441749f9944cb3d12ede1b49d8a1fdf75bf8a8f
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2017-06-09 04:40:24 -0300

    style(codestyle): Apply the eslint rules
    Defined by the `.eslintrc.yml` file

--- git/git.js
+++ git/git.js
@@ -1,66 +1,61 @@
+const path = require('path');
+const git = require('simple-git');
+const fs = require('fs');
+const exec = require('child_process').exec;
 
- var path = require("path");
- var git = require('simple-git');
- var fs = require('fs');
- var exec = require('child_process').exec;
+module.exports = (RED) => {
+  function gitNode(n) {
+    RED.nodes.createNode(this, n);
+  }
+  RED.nodes.registerType('git', gitNode);
 
-module.exports = function(RED) {
-
-    function gitNode(n) {
-       "use strict";
-        RED.nodes.createNode(this,n);
-    };
-
-    RED.nodes.registerType("git",gitNode);
-
-    function commit(message, callback) {
-        
-        //deletes package.json and generates a new one based on node_modules, then commits all changes and pushes them to the remote branch
-        
-        fs.unlink('./package.json', function(err) {
-            if (err) {
-                callback(err,null);
-            } else {
-                exec('npm init -y', function(err, stdout, stderr) {
-                    if (err)    {
-                        callback(err, null)
-                    } else if (stderr) {
-                        callback(stderr, null)
-                    } else {
-                        git().add('--all').commit(message).push('origin','staging', function(err, data) {
-                            if (err) {
-                                callback(err, data)
-                            } else {
-                                callback(null, data)
-                            }
-                        });
-                    }
-                });
-            }
+  function commit(message, callback) {
+    // deletes package.json and generates a new one based on node_modules
+    fs.unlink('./package.json', (err) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        exec('npm init -y', (er, stdout, stderr) => {
+          if (er) {
+            callback(er, null);
+          } else if (stderr) {
+            callback(stderr, null);
+          } else {
+            // commits all changes and pushes them to the remote branch
+            git().add('--all').commit(message).push('origin', 'staging', (e, data) => {
+              if (e) {
+                callback(e, data);
+              } else {
+                callback(null, data);
+              }
+            });
+          }
         });
-    };
-
-    function sendFile(res,filename) {
-        // Use the right function depending on Express 3.x/4.x
-        if (res.sendFile) {
-            res.sendFile(filename);
-        } else {
-            res.sendfile(filename);
-        }
-    }
-
-    RED.httpAdmin.get('/git-ui/git-ui.html', function(req, res){
-        var filename = path.join(__dirname , 'git-ui','git-ui.html');
-        sendFile(res,filename);
-    });  
-
-    RED.httpAdmin.post("/git-ui/commit", function (req, res) {
-        commit(req.body.comment, function(err, data) {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.status(200).send({status:"OK"});
-            }
-        });
+      }
     });
-}
+  }
+
+  function sendFile(res, filename) {
+    // Use the right function depending on Express 3.x/4.x
+    if (res.sendFile) {
+      res.sendFile(filename);
+    } else {
+      res.sendfile(filename);
+    }
+  }
+
+  RED.httpAdmin.get('/git-ui/git-ui.html', (req, res) => {
+    const filename = path.join(__dirname, 'git-ui', 'git-ui.html');
+    sendFile(res, filename);
+  });
+
+  RED.httpAdmin.post('/git-ui/commit', (req, res) => {
+    commit(req.body.comment, (err) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send({ status: 'OK' });
+      }
+    });
+  });
+};
